34e989aa72492be41d25bcdd0894706f5eeb4190
khos0019 <khos0019@algonquinlive.com>
2020-06-22 19:05:05 -0400

    Assignment 1

--- src/main/java/logic/LogicFactory.java
+++ src/main/java/logic/LogicFactory.java
@@ -1,5 +1,6 @@
 package logic;
 
+import java.lang.reflect.InvocationTargetException;
 
 /**
  *
@@ -15,34 +16,7 @@ public abstract class LogicFactory<T> {
     }
   
     public static <T> T getFor(String entityName){
-        StringBuilder sb = new StringBuilder();
         
-        sb.append(Character.toUpperCase(entityName.charAt(0)));
-        
-        if (entityName.length() > 1) {
-            sb.append(entityName.substring(1, entityName.length()).toLowerCase());
-        }
-        
-        String entityClass = sb.toString();
-        
-        String logicClass = entityClass + SUFFIX;
-        
-    }
-    
-   
-    public static <T> T getFor(Class<T> type){
-        try{
-           return type.newInstance();
-           
-        } catch(Exception e){
-            e.printStackTrace();
-        }
-       return null;
-    }
-    
-        
- /*   public T getFor(String entityName) {
-
         String[] parts = entityName.split("_");
         String className = "";
         for (String part : parts) {
@@ -50,16 +24,39 @@ public abstract class LogicFactory<T> {
                     part.substring(1).toLowerCase();
         }
         System.out.println("Class name: " + className);
-
+        
         try {
             Class<?> genericClass = Class.forName(PACKAGE + "." + className + SUFFIX);
             return getFor((Class<T>) genericClass);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+        } catch(IllegalArgumentException ex){
+            ex.printStackTrace();
         }
         return null;
     }
- */ 
+    
+   
+    public static <T> T getFor(Class<T> type){
+        try{
+           return type.getDeclaredConstructor().newInstance();    
+        } catch(InstantiationException e){
+            e.printStackTrace();
+        } catch(IllegalAccessException e){
+            e.printStackTrace();
+        } catch(IllegalArgumentException e){
+            e.printStackTrace();
+        } catch(InvocationTargetException e){
+            e.printStackTrace();
+        } catch(NoSuchMethodException e){
+            e.printStackTrace();
+        } catch(SecurityException e){
+            e.printStackTrace();
+        }
+       return null;
+    }
+     
+ 
 
     //this is just a place holder to keep the code working.
 /*    public static AccountLogic getFor(String account) {
